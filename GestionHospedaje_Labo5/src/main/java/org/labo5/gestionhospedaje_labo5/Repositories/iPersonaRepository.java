246b0ef9de34b828439f18f1335466b05a48cf25
elKelvinRivas <00083522@uca.edu.sv>
2025-05-11 11:35:47 -0600

    Add: Persona Entity and Repository

---
+++ GestionHospedaje_Labo5/src/main/java/org/labo5/gestionhospedaje_labo5/Repositories/iPersonaRepository.java
@@ -0,0 +1,24 @@
+package org.labo5.gestionhospedaje_labo5.Repositories;
+
+import org.labo5.gestionhospedaje_labo5.Domain.Entities.Persona;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface iPersonaRepository extends iGenericRepository<Persona, Integer>{
+
+    //JPA
+    public Persona findPersonaByIdPersona(Integer idPersona);
+
+    public Persona findPersonaByNombre(String nombre);
+
+    public Persona findPersonaByApellido(String apellido);
+
+    public Persona findPersonaByUsuario(String usuario);
+
+    public Persona findPersonaByPassword(String password);
+
+    //Querys hibridas
+    @Query("SELECT u FROM Persona u WHERE u.rol.nombre= :nombre")
+    public Persona findByRolNombre(@Param("nombre") String nombre);
+
+}

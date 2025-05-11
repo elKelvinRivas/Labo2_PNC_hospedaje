34578ba87d616eb5fb8f08c98c1e1ff0e039fada
elKelvinRivas <00083522@uca.edu.sv>
2025-05-11 11:16:30 -0600

    Add: Habitacion Entity and Repository

---
+++ GestionHospedaje_Labo5/src/main/java/org/labo5/gestionhospedaje_labo5/Domain/Entities/Habitacion.java
@@ -0,0 +1,33 @@
+package org.labo5.gestionhospedaje_labo5.Domain.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "habitacion")
+public class Habitacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idHabitacion;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private Integer numero;
+
+    @Column
+    private String descripcion;
+
+    //Muchas habitaciones, pueden estar en un nivel
+    @ManyToOne
+    @JoinColumn(name = "id_nivel", nullable = false, foreignKey = @ForeignKey(name = "FK_habitacion_nivel"))
+    private Nivel nivel;
+
+}

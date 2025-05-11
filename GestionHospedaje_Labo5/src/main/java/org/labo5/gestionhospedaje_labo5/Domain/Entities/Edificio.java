d0627d6bac9a4542a8f6936fc7b26477ab01bb86
elKelvinRivas <00083522@uca.edu.sv>
2025-05-10 22:29:47 -0600

    Add: PropertiesFile :memo:

---
+++ GestionHospedaje_Labo5/src/main/java/org/labo5/gestionhospedaje_labo5/Domain/Entities/Edificio.java
@@ -0,0 +1,4 @@
+package org.labo5.gestionhospedaje_labo5.Domain.Entities;
+
+public class Edificio {
+}
---
+++ GestionHospedaje_Labo5/src/test/java/org/labo5/gestionhospedaje_labo5/GestionHospedajeLabo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.labo5.gestionhospedaje_labo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHospedajeLabo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
